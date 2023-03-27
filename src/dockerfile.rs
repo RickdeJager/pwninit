b6a7bd037f72f181362e05d61e54ed5e0457e9bd
RickdeJager <rickdejager99@gmail.com>
2023-03-27 20:58:25 +0200

    Use ldconfig to find libc in the a docker image

--- src/dockerfile.rs
+++ src/dockerfile.rs
@@ -1,7 +1,11 @@
 use colored::Colorize;
+use regex::Regex;
 
 use std::path::Path;
 use std::process::Command;
+use std::collections::HashMap;
+
+/// Helper functions to pull ld and libc files from a docker container.
 
 pub fn scan_dockerfile(dockerfile: &Path) -> Result<String, String> {
     // Read the dockerfile from the given path
@@ -16,6 +20,77 @@ pub fn scan_dockerfile(dockerfile: &Path) -> Result<String, String> {
         .nth(1)
         .expect("failed to find tag in FROM");
     Ok(docker_tag.to_string())
+}
+
+// TODO: This implicitly assumes that each name is unique, which is not necessarily true.
+//       For example, if you install 32 bit libraries, you will end up with multiple libc versions.
+//
+//       In a docker base image, this may not be an issue. This assumption is fine for images like
+//       ubuntu and archlinux.
+fn parse_ldconfig(content: &str) -> HashMap<&str, &str> {
+    let r = Regex::new(r"\s+(?P<name>[\w.\-]+)\s\(.+\) => (?P<path>[\w/.\-]+)$").unwrap();
+    let mut paths = HashMap::new();
+    for line in content.lines() {
+        if let Some(caps) = r.captures(line) {
+            let lib = caps.name("name").unwrap().as_str();
+            let path = caps.name("path").unwrap().as_str();
+            paths.insert(lib, path);
+        }
+    }
+    paths
+}
+
+fn docker_get_paths(tag: &str, wanted: Vec<&str>) -> Vec<String> {
+    let proc = Command::new("docker")
+        .arg("run")
+        .arg("--rm")
+        .arg(tag)
+        .arg("ldconfig")
+        .arg("-p")
+        .output()
+        .expect("failed to execute docker create");
+    let output = String::from_utf8(proc.stdout).expect("failed to parse docker output");
+    let paths = parse_ldconfig(&output);
+
+    wanted
+        .iter()
+        .map(|lib| paths.get(lib))
+        .filter_map(|p| p.map(|p| p.to_string()))
+        .collect()
+}
+
+fn docker_copy_file(container_id: &str, path: &str, silent: bool) {
+    let copy_proc_libc = Command::new("docker")
+        .arg("cp")
+        .arg("-L")
+        .arg(format!("{}:{}", container_id, path))
+        .arg(".")
+        .output()
+        .expect("failed to execute docker cp");
+
+    // Check if the copy was successful by reading the stderr
+    let stderr = String::from_utf8(copy_proc_libc.stderr).unwrap();
+    if !stderr.is_empty() {
+        if !silent {
+            println!(
+                "{}",
+                format!(
+                    "Failed to extract {} from the docker container.\nError: {}",
+                    path.bold(),
+                    stderr,
+                ).red()
+            );
+        }
+        return;
+    }
+
+    println!(
+        "{}",
+        format!(
+            "Extracted {} from the docker container.",
+            path.bold(),
+        ).green()
+    );
 }
 
 pub fn download_libc_ld_for_docker_tag(tag: &str) -> Result<(), String> {
@@ -44,32 +119,22 @@ pub fn download_libc_ld_for_docker_tag(tag: &str) -> Result<(), String> {
     }
 
     let container_id = String::from_utf8(container_proc.stdout).unwrap().trim().to_string();
+    let paths = docker_get_paths(&tag, vec!["libc.so.6", "ld-linux-x86-64.so.2"]);
 
-    // Now we can copy the linker and libc from the container.
-    // TODO: For now I'm assuming them to be at /lib/ld-linux-x86-64.so.2 and /lib/x86_64-linux-gnu/libc.so.6
-    //
-    //       The correct way to do this to run docker exec in the container and run ldconfig -p
-    //          ldconfig -p | grep ld-
-    //          ldconfig -p | grep libc.so.6
-    //       The current implementation will not work for arch or alpine images, for example.
-    //
-    //       docker exec 5a90d014148d bash -c 'ldconfig -p | grep libc.so.6'
-    //
-    // TODO: Docker cp apparently doesn't throw proper exit codes, so we want to read stdout/err
-    // here and check for errors manually.
-    let copy_proc_ld = Command::new("docker")
-        .arg("cp")
-        .arg("-L")
-        .arg(format!("{}:/lib/x86_64-linux-gnu/ld-linux-x86-64.so.2", container_id))
-        .arg(".")
-        .output();
+    // Copy the files we _know_ to be in the container.
+    paths.iter().for_each(|path| {
+        docker_copy_file(&container_id, path, false);
+    });
 
-    let copy_proc_libc = Command::new("docker")
-        .arg("cp")
-        .arg("-L")
-        .arg(format!("{}:/lib/x86_64-linux-gnu/libc.so.6", container_id))
-        .arg(".")
-        .output();
+    // Add some more paths manually, since we can't get alpine paths from ldconfig for example.
+    let additional_paths = vec![
+        "/lib/ld-musl-x86_64.so.1",
+        "/lib/libc.musl-x86_64.so.1",
+    ];
+
+    additional_paths.iter().for_each(|path| {
+        docker_copy_file(&container_id, path, true);
+    });
 
     // Clean up the container we just created
     let _rm_proc = Command::new("docker")
@@ -77,16 +142,6 @@ pub fn download_libc_ld_for_docker_tag(tag: &str) -> Result<(), String> {
         .arg(container_id)
         .output()
         .expect("failed to execute docker rm");
-
-    let copy_proc_ld = copy_proc_ld.expect("failed to execute docker cp for ld");
-    if !copy_proc_ld.status.success() {
-        return Err("failed to execute docker cp for ld".to_string());
-    }
-
-    let copy_proc_libc = copy_proc_libc.expect("failed to execute docker cp for libc");
-    if !copy_proc_libc.status.success() {
-        return Err("failed to execute docker cp for libc".to_string());
-    }
 
     Ok(())
 }
